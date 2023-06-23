470501fd34ac420e76c9a2b0551bbc228da8adf4
Diogo <diogorocha12@live.com>
2023-06-23 10:08:10 -0300

    feat: separando arquivos e criando controllers para cada um, aplicando testes e2e para o fluxo novo

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 
-import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './environments'
 import fastifyJwt = require('@fastify/jwt')
+import { usersRoutes } from './http/controllers/users/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 // criando uma instância do fastify
 export const app = fastify()
@@ -12,7 +14,9 @@ export const app = fastify()
 app.register(fastifyJwt, { secret: env.JWT_SECRET })
 
 // registrando as rotas
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 // registrando o handler de erros
 app.setErrorHandler((error, _, reply) => {
---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { FastifyInstance } from 'fastify'
+import { authenticateController } from './authenticate.controller'
+import { profileController } from './profile.controller'
+import { registerController } from './register.controller'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', registerController)
+  app.post('/sessions', authenticateController)
+
+  // Authenticated
+  app.get('/me', { onRequest: [verifyJwt] }, profileController)
+}
